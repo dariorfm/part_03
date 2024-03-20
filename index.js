690579753dadc926ed6c28d04d5dd3c7ddf7649c
dariorfm <dariorfm@gmail.com>
2024-03-20 19:48:22 +0100

    3.5_step_5 Random Id

--- index.js
+++ index.js
@@ -1,27 +1,29 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+
 let persons = [
     { 
-        "id": 1,
-        "name": "Arto Hellas", 
-        "number": "040-123456"
-      },
-      { 
-        "id": 2,
-        "name": "Ada Lovelace", 
-        "number": "39-44-5323523"
-      },
-      { 
-        "id": 3,
-        "name": "Dan Abramov", 
-        "number": "12-43-234345"
-      },
-      { 
-        "id": 4,
-        "name": "Mary Poppendieck", 
-        "number": "39-23-6423122"
-      }
+    "id": 1,
+    "name": "Arto Hellas", 
+    "number": "040-123456"
+    },
+    { 
+    "id": 2,
+    "name": "Ada Lovelace", 
+    "number": "39-44-5323523"
+    },
+    { 
+    "id": 3,
+    "name": "Dan Abramov", 
+    "number": "12-43-234345"
+    },
+    { 
+    "id": 4,
+    "name": "Mary Poppendieck", 
+    "number": "39-23-6423122"
+    }
 ];
 
 
@@ -55,6 +57,20 @@ app.delete('/api/persons/:id', (req, res) => {
     persons = persons.filter(person => person.id !== id);
     res.status(204).end();
 });
+
+app.post('/api/persons', (req, res) => {
+    const body = req.body;
+    const person = {
+        // random id
+        id: Math.floor(Math.random() * 1000),
+        name: body.name,
+        number: body.number
+    };
+    persons = persons.concat(person);
+    res.json(person);
+  
+});
+
 
 
 const PORT = 3001;
