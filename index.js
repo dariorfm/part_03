cd97e33e405860ea643f204fb5b258a6c45affa9
dariorfm <dariorfm@gmail.com>
2024-03-21 11:13:30 +0100

    3.8_step_8 Custom token

--- index.js
+++ index.js
@@ -2,10 +2,16 @@ const express = require('express');
 const morgan = require('morgan');
 const app = express();
 
+morgan.token('body', (req) => {
+    return JSON.stringify(req.body);
+});
 
 app.use(express.json());
-app.use(morgan('tiny'));
+//app.use(morgan('tiny'));
+app.use(morgan(':method :url :status :response-time ms :body'));
 
+
+// Resource
 let persons = [
     { 
     "id": 1,
@@ -30,7 +36,7 @@ let persons = [
 ];
 
 
-
+//Routes
 app.get('/', (req, res) => {
     res.send('<h1>Hello World, I am ready to code!</h1>');
 });
