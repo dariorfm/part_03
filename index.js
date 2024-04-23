6e057d8fd430efbe861e2f6c950923f97c262418
dariorfm <dariorfm@gmail.com>
2024-04-23 19:47:54 +0200

    3.17_step_5 Backen updated with HTTP PUT request

--- index.js
+++ index.js
@@ -109,6 +109,19 @@ app.post('/api/persons', (req, res) => {
   
 });
 
+app.put('/api/persons/:id', (req, res, next) => {
+    const body = req.body;
+    const person = {
+        name: body.name,
+        number: body.number
+    };
+    Person.findByIdAndUpdate(req.params.id, person, { new: true })
+        .then(updatedPerson => {
+            res.json(updatedPerson);
+        })
+        .catch(error => next(error));
+});
+
 app.use(unknownEndpoint);
 
 const errorHandler = (error, req, res, next) => {
