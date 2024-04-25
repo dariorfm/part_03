7a5d52501fb3da2bd704c1c36e9506837e900fe9
dariorfm <dariorfm@gmail.com>
2024-04-25 19:57:51 +0200

    3.20_step_8 Person schema updated with validation settings

--- models/person.js
+++ models/person.js
@@ -12,7 +12,17 @@ mongoose.connect(url).then(result => {
 
 const personSchema = new mongoose.Schema({
     name: {type: String, minLength: 3, required: true, unique: true},
-    number: String
+    number: {
+        type: String,
+        required: true,
+        minLength: 8,
+        validate: {
+            validator: function(v) {
+                return /\d{2,3}-\d/.test(v);
+            },
+            message: props => `${props.value} is not a valid phone number!`
+        },
+    }
 });
 
 personSchema.set('toJSON', {
