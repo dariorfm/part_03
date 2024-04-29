efee328e6bb8af047c892a56c5143f052f721623
dariorfm <dariorfm@gmail.com>
2024-04-29 15:23:16 +0200

    Code fixed

--- index.js
+++ index.js
@@ -1,145 +1,141 @@
-require('dotenv').config();
-const express = require('express');
-const morgan = require('morgan');
-const cors = require('cors');
-const app = express();
-const Person = require('./models/person');
+require('dotenv').config()
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+const app = express()
+const Person = require('./models/person')
 
 const unknownEndpoint = (req, res) => {
-    res.status(404).send({ error: 'unknown endpoint' })
-};
+  res.status(404).send({ error: 'unknown endpoint' })
+}
 
 morgan.token('body', (req) => {
-    return JSON.stringify(req.body);
-});
+  return JSON.stringify(req.body)
+})
 
-app.use(express.static('dist'));
-app.use(express.json());
-//app.use(morgan('tiny'));
-app.use(cors());
-app.use(morgan(':method :url :status :response-time ms :body'));
-
+app.use(express.static('dist'))
+app.use(express.json())
+// app.use(morgan('tiny'));
+app.use(cors())
+app.use(morgan(':method :url :status :response-time ms :body'))
 
 // Resource
-let persons = [
-    { 
-    "id": 1,
-    "name": "Arto Hellas", 
-    "number": "040-123456"
-    },
-    { 
-    "id": 2,
-    "name": "Ada Lovelace", 
-    "number": "39-44-5323523"
-    },
-    { 
-    "id": 3,
-    "name": "Dan Abramov", 
-    "number": "12-43-234345"
-    },
-    { 
-    "id": 4,
-    "name": "Mary Poppendieck", 
-    "number": "39-23-6423122"
-    }
-];
+const persons = [
+  {
+    id: 1,
+    name: 'Arto Hellas',
+    number: '040-123456'
+  },
+  {
+    id: 2,
+    name: 'Ada Lovelace',
+    number: '39-44-5323523'
+  },
+  {
+    id: 3,
+    name: 'Dan Abramov',
+    number: '12-43-234345'
+  },
+  {
+    id: 4,
+    name: 'Mary Poppendieck',
+    number: '39-23-6423122'
+  }
+]
 
-
-//Routes
+// Routes
 app.get('/', (req, res) => {
-    res.send('<h1>Hello World, I am ready to code!</h1>');
-});
+  res.send('<h1>Hello World, I am ready to code!</h1>')
+})
 
 app.get('/api/persons', (req, res) => {
-    Person.find({}).then(persons => {
-        res.json(persons);
-    });
-});
+  Person.find({}).then(persons => {
+    res.json(persons)
+  })
+})
 
 app.get('/info', (req, res) => {
-    Person.find({}).then(persons => {
-        res.send(`<p>Phonebook has info for ${persons.length} people</p>
-        <p>${new Date()}</p>`);
-    });
-});
+  Person.find({}).then(persons => {
+    res.send(`<p>Phonebook has info for ${persons.length} people</p>
+        <p>${new Date()}</p>`)
+  })
+})
 
 app.get('/api/persons/:id', (req, res, next) => {
-    Person.findById(req.params.id)
-        .then(person => {
-            if (person) {
-                res.json(person);
-            } else {
-                res.status(404).end();
-            }
-        })
-        .catch(error => next(error));   
-    });
+  Person.findById(req.params.id)
+    .then(person => {
+      if (person) {
+        res.json(person)
+      } else {
+        res.status(404).end()
+      }
+    })
+    .catch(error => next(error))
+})
 
 app.delete('/api/persons/:id', (req, res, next) => {
-    Person.findByIdAndDelete(req.params.id)
-        .then(result =>{
-            res.status(204).end();
-        
-        })
-        .catch(error => next(error));
-});
+  Person.findByIdAndDelete(req.params.id)
+    .then(result => {
+      res.status(204).end()
+    })
+    .catch(error => next(error))
+})
 
 app.post('/api/persons', (req, res, next) => {
-    const body = req.body;
-    // name and number are required
-    if (!body.name || !body.number) {
-        return res.status(400).json({
-            error: 'name or number is missing'
-        });
-    }
-    // check if name already exists
-    const nameExists = persons.find(person => person.name === body.name);
-    if (nameExists) {
-        return res.status(400).json({
-            error: 'name must be unique'
-        });
-    }
-    const person = new Person({
-        name: body.name,
-        number: body.number
-    });
-
-    person.save().then(savedPerson => {
-        res.json(savedPerson);
+  const body = req.body
+  // name and number are required
+  if (!body.name || !body.number) {
+    return res.status(400).json({
+      error: 'name or number is missing'
     })
-    .catch(error => next(error));
-  
-});
+  }
+  // check if name already exists
+  const nameExists = persons.find(person => person.name === body.name)
+  if (nameExists) {
+    return res.status(400).json({
+      error: 'name must be unique'
+    })
+  }
+  const person = new Person({
+    name: body.name,
+    number: body.number
+  })
+
+  person.save().then(savedPerson => {
+    res.json(savedPerson)
+  })
+    .catch(error => next(error))
+})
 
 app.put('/api/persons/:id', (req, res, next) => {
-    const body = req.body;
-    const person = {
-        name: body.name,
-        number: body.number
-    };
-    Person.findByIdAndUpdate(req.params.id, person, { new: true })
-        .then(updatedPerson => {
-            res.json(updatedPerson);
-        })
-        .catch(error => next(error));
-});
+  const body = req.body
+  const person = {
+    name: body.name,
+    number: body.number
+  }
+  Person.findByIdAndUpdate(req.params.id, person, { new: true })
+    .then(updatedPerson => {
+      res.json(updatedPerson)
+    })
+    .catch(error => next(error))
+})
 
-app.use(unknownEndpoint);
+app.use(unknownEndpoint)
 
 const errorHandler = (error, req, res, next) => {
-    console.error(error.message);
-    if (error.name === 'CastError') {
-        return res.status(400).send({ error: 'malformatted id' });
-    } else if (error.name === 'ValidationError') {
-        return res.status(400).json({ error: error.message });
-    }
+  console.error(error.message)
+  if (error.name === 'CastError') {
+    return res.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return res.status(400).json({ error: error.message })
+  }
 
-    next(error);
-};
+  next(error)
+}
 
-app.use(errorHandler);
+app.use(errorHandler)
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+  console.log(`Server running on port ${PORT}`)
+})
--- mongo.js
+++ mongo.js
@@ -1,36 +1,36 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 
-const password = process.argv[2];
-const url = `mongodb+srv://dariorfm:${password}@clusterp.yj9mde4.mongodb.net/phoneBookApp?retryWrites=true&w=majority&appName=ClusterP`;
+const password = process.argv[2]
+const url = `mongodb+srv://dariorfm:${password}@clusterp.yj9mde4.mongodb.net/phoneBookApp?retryWrites=true&w=majority&appName=ClusterP`
 
-mongoose.connect(url);
+mongoose.connect(url)
 
 const personSchema = new mongoose.Schema({
-    name: String,
-    number: String
-});
+  name: String,
+  number: String
+})
 
-const Person = mongoose.model('Person', personSchema);
+const Person = mongoose.model('Person', personSchema)
 
 if (process.argv.length === 3) {
-    Person.find({}).then(result => {
-        console.log('Phonebook:');
-        result.forEach(person => {
-            console.log(`${person.name} ${person.number}`);
-        });
-        mongoose.connection.close();
-    });
+  Person.find({}).then(result => {
+    console.log('Phonebook:')
+    result.forEach(person => {
+      console.log(`${person.name} ${person.number}`)
+    })
+    mongoose.connection.close()
+  })
 } else if (process.argv.length === 5) {
-    const person = new Person({
-        name: process.argv[3],
-        number: process.argv[4]
-    });
+  const person = new Person({
+    name: process.argv[3],
+    number: process.argv[4]
+  })
 
-    person.save().then(() => {
-        console.log(`Added ${person.name} ${person.number} to phonebook`);
-        mongoose.connection.close();
-    });
+  person.save().then(() => {
+    console.log(`Added ${person.name} ${person.number} to phonebook`)
+    mongoose.connection.close()
+  })
 } else {
-    console.log('Please provide the password only, or password, name and number as arguments: node mongo.js <password> <name> <number>');
-    process.exit(1);
+  console.log('Please provide the password only, or password, name and number as arguments: node mongo.js <password> <name> <number>')
+  process.exit(1)
 }
